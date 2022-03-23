5fae51f5a1cc4a1bceecfe818290f9c52cc216ce
rokas <rokasra@gmail.com>
2022-03-23 17:25:34 +0200

    chuck N jokes working

--- js/onFetch.js
+++ js/onFetch.js
@@ -1,16 +1,16 @@
 //sukurti random chuck norris juokeli kiekviena karta paspaudus mygtuka. Viska atvaizduoti html'e.
 const btnEl = document.getElementById("btn1");
-const h3El = document.getElementById("joke");
+// const h3El = document.getElementById("joke");
 const base_url = "https://api.chucknorris.io/jokes";
 //______//
-fetch(`${base_url}/random`)
-  .then((response) => response.json())
-  .then((data) => {
-    const joke = data.value;
-    btnEl.addEventListener("click", (event) => {
-      event.preventDefault();
-      console.log("jokeDisplay===", joke);
-      const newH3El = document.createElement("h3");
-      h3El.textContent = joke;
+
+btnEl.addEventListener("click", () => {
+  fetch(`${base_url}/random`)
+    .then((response) => response.json())
+    .then((data) => {
+      let joke = data.value;
+      const h3El = document.createElement("h3");
+      h3El.innerHTML += joke;
+      document.body.append(h3El);
     });
-  });
+});
