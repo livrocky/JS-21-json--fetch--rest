8ac8c888df7692eb67d68ee85778bada0f99513a
rokas <rokasra@gmail.com>
2022-03-23 16:23:06 +0200

    chuck N jokes not finished

--- js/fetchTypicode.js
+++ js/fetchTypicode.js
@@ -2,7 +2,8 @@ const base_url = "https://jsonplaceholder.typicode.com";
 
 //parsiusti ir isconsolinti 100 postu is :  https://jsonplaceholder.typicode.com/posts
 
-console.time("fetch");
+// console.time("fetch");
+
 fetch(`${base_url}/posts`)
   .then((response) => response.json())
   .then((data) => console.log("posts", data))
@@ -24,15 +25,17 @@ fetch(`${base_url}/users`)
   .then((data) => console.log("users", data))
   .catch((error) => console.log(error));
 
-//get all commenst of 5 posts
+//get all comments of 5 posts
+//BUTINAI PASIDARYTI!!//
 
-//BUTINAI PASIBAIGTI!!//
+fetch(`${base_url}/comments`)
+  .then((response) => response.json())
+  .then((data) => {
+    const post5Comments = data.filter((uObj) => uObj.postId === 5);
+    console.log("post5Comments===", post5Comments);
+  });
 
-// fetch(`${base_url}/comments`)
-//   .then((response) => response.json())
-//   .then((data) =>
-
-//// get all users usernames in an array
+// get all users usernames in an array
 
 fetch(`${base_url}/users`)
   .then((response) => response.json())
